c096804c05c30fedd6133d7519b5a5c92f5fbb0c
AliMahmoud80 <ali.mahmoud7080@gmail.com>
2021-09-11 01:11:27 +0200

    fixed previews urls

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::any('/{any?}', function () {
   return view('app');
-})->where('any', '^(?!api\/)[\/\w\.-]*');
+})->where('any', '^(?!(api|storage)\/)[\/\w\.-]*');
